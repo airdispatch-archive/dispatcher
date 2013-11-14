1e9ba8f95a39591e61a57320ed6555a5df7ec1a6
Hunter Leath <h@hunterleath.com>
2013-11-14 14:45:56 -0500

    Updated the Webserver

--- models/models.go
+++ models/models.go
@@ -1,5 +1,6 @@
 package models
 
+<<<<<<< Updated upstream
 import (
 	"airdispat.ch/client/framework"
 	"airdispat.ch/common"
@@ -183,3 +184,188 @@ type Tracker struct { // dispatch_tracker
 	URL     string
 	Address string
 }
+=======
+// import (
+// 	"airdispat.ch/client/framework"
+// 	"airdispat.ch/common"
+// 	library "github.com/airdispatch/go-pressure"
+// 	"github.com/coopernurse/gorp"
+// 	"encoding/hex"
+// 	"errors"
+// 	"bytes"
+// 	"fmt"
+// 	// "log"
+// 	"os"
+// )
+
+// func ConnectToDB() (*gorp.DbMap, error) {
+// 	// serverKey, _ := common.CreateKey()
+// 	db, err := library.OpenDatabaseFromURL(os.Getenv("DATABASE_URL"))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+// 	// dbmap.TraceOn("[gorp]", log.New(os.Stdout, "editor:", log.Lmicroseconds))
+
+// 	dbmap.AddTableWithName(Message{}, "dispatch_messages").SetKeys(true, "Id")
+// 	dbmap.AddTableWithName(Alert{}, "dispatch_alerts").SetKeys(true, "Id")
+// 	dbmap.AddTableWithName(Subscription{}, "dispatch_subscriptions").SetKeys(true, "Id")
+
+// 	dbmap.AddTableWithName(Tracker{}, "dispatch_trackers").SetKeys(true, "Id")
+
+// 	dbmap.AddTableWithName(User{}, "dispatch_users").SetKeys(true, "Id")
+
+// 	return dbmap, nil
+// }
+
+// type User struct { // dispatch_userg
+// 	Salt string
+// 	Username string
+// 	Password string
+// 	FullName string
+// 	Keypair []byte
+// 	Id int64
+// 	Address string
+// 	LoadedKey *common.ADKey `db:"-"` // This field is transient
+// }
+
+// func (u *User) Populate() error {
+// 	keys, err := common.GobDecodeKey(bytes.NewBuffer(u.Keypair))
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	u.LoadedKey = keys
+// 	u.Address = keys.HexEncode()
+
+// 	return nil
+// }
+
+// func CreateUser(username string, password string, s *library.Server) *User {
+// 	key, _ := common.CreateADKey()
+// 	buf := new(bytes.Buffer)
+// 	key.GobEncodeKey(buf)
+
+// 	newUser := &User {
+// 		Username: username,
+// 		Password: HashPassword(password),
+// 		Keypair: buf.Bytes(),
+// 	}
+// 	newUser.Populate()
+
+// 	newUser.RegisterUserWithTracker(s)
+
+// 	return newUser
+// }
+
+// func (u  *User) RegisterUserWithTracker(s *library.Server) error {
+// 	theTrackers, err := GetTrackerList(s.DbMap)
+
+// 	c := &framework.Client {}
+// 	c.Populate(u.LoadedKey)
+// 	c.MailServer = s.Mailserver
+
+// 	// Convert to tracker list
+// 	success := false
+
+// 	for _, v := range(theTrackers) {
+// 		err = c.SendRegistration(v.URL)
+// 		if err == nil {
+// 			success = true
+// 		} else {
+// 			fmt.Println("Got a Tracker Error", err)
+// 		}
+// 	}
+
+// 	if !success {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
+// func GetTrackerList(dbMap *gorp.DbMap) ([]*Tracker, error) {
+// 	var theTrackers []*Tracker
+// 	_, err := dbMap.Select(&theTrackers, "select * from dispatch_trackers")
+// 	if err != nil {
+// 		fmt.Println("SQL Error", err)
+// 		return nil, err
+// 	}
+// 	return theTrackers, nil
+// }
+
+// func GetUserWithAddress(dbMap *gorp.DbMap, address string) (*User, error) {
+// 	var theUsers []*User
+// 	_, err := dbMap.Select(&theUsers, "select * from dispatch_users where address='" + address + "'")
+// 	if err != nil {
+// 		fmt.Println("SQL Error")
+// 		fmt.Println(err)
+// 		return nil, err
+// 	}
+
+// 	if len(theUsers) != 1 {
+// 		return nil, errors.New("Incorrect Number of Rows Returned")
+// 	}
+// 	return theUsers[0], nil
+// }
+
+// func (user *User) VerifyPassword(password string) bool {
+// 	return (user.Password == HashPassword(password))
+// }
+
+// func HashPassword(password string) string {
+// 	return hex.EncodeToString(common.HashSHA([]byte(password)))
+// }
+
+// type Mailbox struct {}
+
+// type Contact struct {
+// 	Id int64
+// 	User int64
+// 	Address string
+// 	Name string
+// }
+
+// type Message struct {
+// 	Id int64
+// 	ToAddress string
+// 	FromAddress string `db:"-"` // This field is transient
+// 	Slug string
+// 	MessageType string
+// 	Timestamp int64
+// 	SendingUser int64
+// 	Content []byte
+// }
+
+// type Alert struct {
+// 	Id int64
+// 	Content []byte
+// 	ToAddress string
+// 	Timestamp int64
+// 	Folder string
+// 	ToUser int64
+// }
+
+// type Stream struct {
+// 	Id int64
+// 	Message string
+// 	LinkedUser string
+// 	LinkedObject string
+// }
+
+// type Subscription struct {
+// 	Id int64
+// 	User int64
+// 	SubscribedAddress string
+// 	Note string
+// }
+
+// type Attatchment struct {}
+
+// type Tracker struct { // dispatch_tracker
+// 	Id int64
+// 	URL string
+// 	Address string
+// }
+>>>>>>> Stashed changes
--- webserver.go
+++ webserver.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"github.com/airdispatch/dispatcher/models"
-	"github.com/airdispatch/dispatcher/views"
-	library "github.com/airdispatch/go-pressure"
+	// "fmt"
+	// "github.com/airdispatch/dispatcher/models"
+	// "github.com/airdispatch/dispatcher/views"
+	"github.com/airdispatch/go-pressure"
 	_ "github.com/lib/pq"
 	"os"
+	"path/filepath"
 	// "airdispat.ch/common"
 )
 
 var flag_port = flag.String("port", "2048", "specify the port that the server should run on")
-var db_flush = flag.Bool("db_flush", false, "specifies to flush the database")
-var db_create = flag.Bool("db_create", false, "specifies to create the database")
-var me = flag.String("me", "", "specify the location of this running server")
+
+// var db_flush = flag.Bool("db_flush", false, "specifies to flush the database")
+// var db_create = flag.Bool("db_create", false, "specifies to create the database")
+
+// var me = flag.String("me", "", "specify the location of this running server")
 
 func main() {
 	flag.Parse()
@@ -24,109 +27,81 @@ func main() {
 		temp_port = *flag_port
 	}
 
-	dbMap, err := models.ConnectToDB()
-	if err != nil {
-		fmt.Println("Can't Connect to DB")
-		fmt.Println(err)
-		return
-	}
+	// dbMap, err := models.ConnectToDB()
+	// if err != nil {
+	// 	fmt.Println("Can't Connect to DB")
+	// 	fmt.Println("err")
+	// 	return
+	// }
 
-	theServer := &library.Server{
-		Port:            temp_port,
-		DbMap:           dbMap,
-		MainSessionName: "dispatcher-session",
-		Mailserver:      *me,
-	}
+	temp_wd, _ := os.Getwd()
 
-	// Flush the Database
-	if *db_flush {
-		theServer.DbMap.DropTables()
-	}
+	// Get Relevant Paths
+	template_dir := filepath.Join(temp_wd, "templates")
+	static_dir := filepath.Join(temp_wd, "static")
 
-	// Create the Database
-	if *db_create {
-		// Create Tables
-		err := theServer.DbMap.CreateTablesIfNotExists()
-		if err != nil {
-			fmt.Println("Problem Creating Tables")
-			fmt.Println(err)
-			return
-		}
+	s := pressure.CreateServer(temp_port, true)
+	t_eng := s.CreateTemplateEngine(template_dir, "base.html")
 
-		// The Tracker
-		fmt.Println("Time to setup the default Trackers")
+	// Register URLs
+	s.RegisterURL(
+		// pressure.NewURLRoute("^/project/airdispatch", &ProjectController{tEng}),
+		pressure.NewURLRoute("^/$", &HomepageController{t_eng}),
+		pressure.NewStaticFileRoute("^/static/", static_dir),
+	)
 
-		for {
-			var t_url, t_address string
-			fmt.Print("Tracker URL (or 'done' to stop): ")
-			fmt.Scanln(&t_url)
-
-			if t_url == "done" {
-				break
-			}
-
-			fmt.Print("Tracker Address: ")
-			fmt.Scanln(&t_address)
-
-			t_full := &models.Tracker{URL: t_url, Address: t_address}
-			theServer.DbMap.Insert(t_full)
-		}
-		// Create the User
-		fmt.Println("Let's create the first user.")
-
-		var username, password, first, last string
-
-		fmt.Print("Username (no spaces): ")
-		fmt.Scanln(&username)
-
-		fmt.Print("Password: ")
-		fmt.Scanln(&password)
-
-		fmt.Print("First Name: ")
-		fmt.Scanln(&first)
-		fmt.Print("Last Name: ")
-		fmt.Scanln(&last)
-
-		newUser := models.CreateUser(username, password, theServer)
-		newUser.FullName = (first + " " + last)
-
-		fmt.Println("New User Address", newUser.Address)
-		theServer.DbMap.Insert(newUser)
-	}
-
-	theServer.ConfigServer()
-	defineRoutes(theServer)
-
-	theServer.RunServer()
+	s.RunServer()
 }
 
-func defineRoutes(s *library.Server) {
-	s.WebServer.Get("/", views.HomePage(s, views.Dashboard(s)))
-
-	s.WebServer.Get("/compose", views.TemplateLoginRequired(s, s.DisplayTemplate("compose.html")))
-	s.WebServer.Post("/compose", views.TemplateLoginRequired(s, views.CreateMessage(s)))
-
-	s.WebServer.Get("/subscribe", views.TemplateLoginRequired(s, views.ShowSubscriptions(s)))
-	s.WebServer.Post("/subscribe", views.TemplateLoginRequired(s, views.CreateSubscription(s)))
-
-	s.WebServer.Get("/inbox", views.TemplateLoginRequired(s, views.ShowFolder(s, "Inbox")))
-	s.WebServer.Get("/profile", views.TemplateLoginRequired(s, views.ShowFolder(s, "Profile")))
-
-	s.WebServer.Get("/alert/([0-9]*)", views.WildcardTemplateLoginRequired(s, views.ShowAlert(s)))
-
-	s.WebServer.Get("/message/([0-9]*)", views.WildcardTemplateLoginRequired(s, views.ShowMessage(s)))
-
-	s.WebServer.Get("/message/([0-9]*)/edit", views.WildcardTemplateLoginRequired(s, views.DisplayEditMessage(s)))
-	s.WebServer.Post("/message/([0-9]*)/edit", views.WildcardTemplateLoginRequired(s, views.UpdateMessage(s)))
-
-	s.WebServer.Get("/message/([0-9]*)/delete", views.WildcardTemplateLoginRequired(s, views.DeleteMessage(s)))
-
-	s.WebServer.Get("/account/tracker_registration", views.TemplateLoginRequired(s, views.RegisterWithTrackers(s)))
-
-	s.WebServer.Get("/login", s.DisplayTemplate("login.html"))
-	s.WebServer.Post("/login", views.LoginView(s))
-
-	s.WebServer.Post("/account/create", views.RegisterUser(s))
-
-	s.WebServer.Get("/logout", views.TemplateLoginRequired(s, views.LogoutView(s)))
+type HomepageController struct {
+	templates *pressure.TemplateEngine
 }
+
+func (c *HomepageController) GetResponse(p *pressure.Request, l *pressure.Logger) (pressure.View, *pressure.HTTPError) {
+	return c.templates.NewTemplateView("dashboard.html", nil), nil
+}
+
+// <<<<<<< Updated upstream
+// func defineRoutes(s *library.Server) {
+// 	s.WebServer.Get("/", views.HomePage(s, views.Dashboard(s)))
+// =======
+// // func defineRoutes(s *library.Server) {
+// // 	s.WebServer.Get("/", views.TemplateLoginRequired(s, views.Dashboard(s)))
+// >>>>>>> Stashed changes
+
+// // 	s.WebServer.Get("/compose", views.TemplateLoginRequired(s, s.DisplayTemplate("compose.html")))
+// // 	s.WebServer.Post("/compose", views.TemplateLoginRequired(s, views.CreateMessage(s)))
+
+// // 	s.WebServer.Get("/subscribe", views.TemplateLoginRequired(s, views.ShowSubscriptions(s)))
+// // 	s.WebServer.Post("/subscribe", views.TemplateLoginRequired(s, views.CreateSubscription(s)))
+
+// <<<<<<< Updated upstream
+// 	s.WebServer.Get("/inbox", views.TemplateLoginRequired(s, views.ShowFolder(s, "Inbox")))
+// 	s.WebServer.Get("/profile", views.TemplateLoginRequired(s, views.ShowFolder(s, "Profile")))
+
+// 	s.WebServer.Get("/alert/([0-9]*)", views.WildcardTemplateLoginRequired(s, views.ShowAlert(s)))
+
+// 	s.WebServer.Get("/message/([0-9]*)", views.WildcardTemplateLoginRequired(s, views.ShowMessage(s)))
+// =======
+// // 	s.WebServer.Get("/inbox", views.TemplateLoginRequired(s, views.ShowFolder(s, "Inbox")))
+// // 	s.WebServer.Get("/sent", views.TemplateLoginRequired(s, views.ShowFolder(s, "Sent Messages")))
+
+// // 	s.WebServer.Get("/alert/([0-9]*)", views.WildcardTemplateLoginRequired(s, views.ShowAlert(s)))
+// >>>>>>> Stashed changes
+
+// 	s.WebServer.Get("/message/([0-9]*)", views.WildcardTemplateLoginRequired(s, views.ShowMessage(s)))
+
+// 	s.WebServer.Get("/message/([0-9]*)/edit", views.WildcardTemplateLoginRequired(s, views.DisplayEditMessage(s)))
+// 	s.WebServer.Post("/message/([0-9]*)/edit", views.WildcardTemplateLoginRequired(s, views.UpdateMessage(s)))
+
+// 	s.WebServer.Get("/message/([0-9]*)/delete", views.WildcardTemplateLoginRequired(s, views.DeleteMessage(s)))
+
+// 	s.WebServer.Get("/account/tracker_registration", views.TemplateLoginRequired(s, views.RegisterWithTrackers(s)))
+
+// 	s.WebServer.Get("/login", s.DisplayTemplate("login.html"))
+// 	s.WebServer.Post("/login", views.LoginView(s))
+
+// 	s.WebServer.Post("/account/create", views.RegisterUser(s))
+
+// 	s.WebServer.Get("/logout", views.TemplateLoginRequired(s, views.LogoutView(s)))
+// }
